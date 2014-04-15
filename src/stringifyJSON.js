fa69cce31e68a5427ac4c1f7023da0940ca8b6c5
Hack Reactor Students <talegram@gmail.com>
2014-04-14 20:17:16 -0700

    refactored commas to joins

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,12 +2,17 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+
+var isStringifiable = function (item) {
+  return item !== undefined && typeof item !== 'function';
+}
+
 var stringifyJSON = function (obj) {
   // Create initial string
   var result = "";
 
   // if its not a function or an undefined value
-  if (obj !== undefined && typeof obj !== 'function') {
+  if (isStringifiable(obj)) {
     if (typeof obj === 'number' ) {
       result += obj;
     } else if ( typeof obj === 'string' ) {
@@ -17,41 +22,21 @@ var stringifyJSON = function (obj) {
     } else if ( obj === null) {
       result += "null";
     } else if ( Array.isArray(obj) ) {
-      // var temp = [];
-      // for ( var i = 0; i < obj.length; i++ ) {
-      //   if (obj[i] !== undefined && typeof obj[i] !== 'function') {
-      //     temp.push(obj[i]);
-      //   }
-      // }
-      // obj = temp;
-      result += "[";
-      var counter = 0;
-      for (var i = 1; i < obj.length; i++) {
-        if ( obj[i] !== undefined && typeof obj[i] !== 'function' ) {
-          result += stringifyJSON(obj[i]) + ",";
-          counter++;
+      var temp = [];
+      for (var i = 0; i < obj.length; i++) {
+        if ( isStringifiable(obj[i]) ) {
+          temp.push( stringifyJSON(obj[i]) );
         }
       }
-      if (counter > 0) {
-        result = result.substr(0, result.length - 1);
-      }
-      // [9,] -> [[0-9],]
-      //result = result.substr(0, result.length - 1);
-      result += "]";
+      result += "[" + temp.join(",") + "]";
     } else if (typeof obj === 'object') {
-      result += "{";
-      var counter = 0;
+      var temp = [];
       for ( var key in obj ) {
-        if (obj[key] !== undefined && typeof obj[key] !== 'function') {
-          result += "\"" + key + "\":";
-          result += stringifyJSON(obj[key]) + ",";
-          counter++;
+        if ( isStringifiable(obj[key]) ) {
+          temp.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
         }
       }
-      if (counter > 0) {
-        result = result.substr(0, result.length - 1);
-      }
-      result += "}";
+      result += "{" + temp.join(",") + "}";
     }
 
   }
