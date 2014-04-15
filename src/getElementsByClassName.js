c1b0ce5f4a948542aa607f58001e36fc804dc226
Hack Reactor Students <talegram@gmail.com>
2014-04-14 20:51:14 -0700

    refactored getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,34 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// TODO: hasClass method
+var hasClass = function(node, className) {
+  return node.className.split(" ").indexOf(className) !== -1;
+}
+
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
+var getElementsByClassName = function (className, node) {
   // your code here
+  // Create the storage
+  var results = [];
+
+  // Set the node
+  node = node || document.body;
+
+  // check if the node has class
+  //  if it does, then add it to the storage
+  if ( hasClass(node, className) ) {
+    results.push(node);
+  }
+
+  // For all of the children
+  //  run getElementsByClassName
+  if ( node.children ) {
+    for (var i = 0; i < node.children.length; i++) {
+      results = results.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+
+  // return the results
+  return results;
 };
